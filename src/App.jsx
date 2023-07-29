cc99efeb1708303f784a9b7e026d09b2ab58f87f
tuankthai <ttlive4jc@gmail.com>
2023-07-28 18:15:13 -0700

    import data.js to react useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
+
+
 
 function App() { 
   
+  // console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
   return (
 
-      <div>
-        
+    <div>{
+      puppies.map((puppy) => {
+        return <p key = {puppy.id}>{puppy.name}</p>
+      })
+    }        
       </div>
       
     
