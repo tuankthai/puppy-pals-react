ad226876ca01ffd808508ed1b5901e50b0b02b73
tuankthai <ttlive4jc@gmail.com>
2023-07-28 19:06:31 -0700

    everything is working except wrap up tab

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,46 @@
 // import './App.css'
 import { puppyList } from './data.js'
 import { useState } from 'react'
+import './App.css'
 
 
 
-function App() { 
-  
+
+function App() {
+
   // console.log(puppyList)
   const [puppies, setPuppies] = useState(puppyList)
   console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
 
-    <div>{
-      puppies.map((puppy) => {
-        return <p key = {puppy.id}>{puppy.name}</p>
-      })
-    }        
-      </div>
-      
-    
+    <div className="puppyList">
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+
+      {featPupId &&
+        // <p>{featPupId}</p>
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      }
+
+
+
+    </div>
+
+
   )
 }
 
