fde31bf9689d934eb0723eab5a69560929634dad
tuankthai <ttlive4jc@gmail.com>
2023-07-28 20:22:32 -0700

    adjust config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/puppy-pals-react/",
   plugins: [react()],
-  base: "/puppy-pals-react/"
 })
